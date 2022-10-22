6ad08b7110ee791dbcde86f894b219bc7e9ffed8
MichinariNukazawa <michinari.nukazawa@gmail.com>
2022-10-22 15:08:13 +0900

    sort tag

--- event.js
+++ event.js
@@ -100,28 +100,48 @@ function onSelectedTab(tab)
 
 		const charas = response.collected_tagst.characters
 		const genes = response.collected_tagst.generals
-		let tags =  charas.concat(genes)
+		let tagarrays = sortingTags(genes)
+		tagarrays.unshift(charas)
+		console.log(tagarrays)
 
-		for( let i = 0; i < tags.length; i++){
-			// プロンプトでは括弧は強弱指定となるためタグの括弧をエスケープする
-			// '{}'は(おそらく)タグに含まれないのでしていない
-			// '[]'もいまのところ見かけていないが念のため
-			tags[i] = tags[i].replaceAll('(', '\\(')
-			tags[i] = tags[i].replaceAll(')', '\\)')
-			tags[i] = tags[i].replaceAll('[', '\\[')
-			tags[i] = tags[i].replaceAll(']', '\\]')
-		}
+		tagarrays.forEach((tagarr, index) => {
+			const newtagarr = tagarr.map((tag) => {
+				// プロンプトでは括弧は強弱指定となるためタグの括弧をエスケープする
+				// '{}'は(おそらく)タグに含まれないのでしていない
+				// '[]'もいまのところ見かけていないが念のため
+				tag = tag.replaceAll('(', '\\(')
+				tag = tag.replaceAll(')', '\\)')
+				tag = tag.replaceAll('[', '\\[')
+				tag = tag.replaceAll(']', '\\]')
+				return tag
+			})
+			tagarrays[index] = newtagarr
+		})
 
 		let s = ''
 		switch(myconf.targetKind){
 		case 'diffusion':
-			for( let i = 0; i < tags.length; i++){
-				tags[i] = tags[i].replaceAll(' ', '_')
-			}
-			s = tags.join(' ')
+		{
+			tagarrays = tagarrays.map((tagarr) => {
+				return tagarr.map((tag) => {
+					return tag.replaceAll(' ', '_')
+				})
+			})
+
+			const ss = tagarrays.map((tagarr) => {
+				return tagarr.join(' ')
+			})
+			// Tagグループ間は２つ開ける
+			// 基本的にはデバッグのための挙動
+			// プロンプトに空白を増やしても画像生成に副作用はないはず
+			s = ss.join('  ')
+		}
 			break
 		case 'novelai':
-			s = tags.join(', ')
+			const ss = tagarrays.map((tagarr) => {
+				return tagarr.join(', ')
+			})
+			s = ss.join(',  ')
 			break
 		default:
 			showErrorMsg('BUG invalid:' + myconf.targetKind)
@@ -168,5 +188,436 @@ chrome.contextMenus.onClicked.addListener((item) => {
 	}
 })
 
-}
+// ******** Ordering Structure Tags ********
+function sortingTags(tags){
+	// ここで並べた順にソートとなる可能性はあるので、並び順は考えて置く
+	// https://danbooru.donmai.us/wiki_pages/tag_groups
+	const bodyWords = [
+		// 尾
+		'tail', 'kyuubi', 'multiple_tails',
+		// 属性
+		// 狐耳少女('fox girl')、猫耳少女等
+		'girl', 'boy', 'kitsune',
+		// 目系
+		'eye', 'sclera', 'pupils',
+		// 耳
+		'ear',
+		// 髪系(髪型等)
+		// https://danbooru.donmai.us/wiki_pages/tag_group%3Ahair_styles
+		'hair', 'bald', 'sidelock', 'cut', 'chonmage', 'top', 'okappa', 'cornrows',
+		'dreadlocks', 'mullet', 'braid', 'bun',
+		'side up', 'nihongami', 'mizura', 'ponytail', 'twintail', 'afro',
+		'bang', 'intake', 'ahoge', 'comb over', 'mohawk', 'blunt ends',
+		// 髪色個別
+		'multicolored hair',
+		'colored inner hair',
+		'gradient hair',
+		'rainbow hair',
+		'split-color hair',
+		'streaked hair',
+		'two-tone hair',
+		'colored tips',
+		// ひげ(髪グループのFacial hair)
+		// https://danbooru.donmai.us/wiki_pages/tag_group%3Ahair
+		'beard', 'goatee', 'mustache','stubble',
+		// 胸（サイズ指定として）
+		// nippleは見える構図系が多いようなので入れていない
+		'breast','chest',
+		// 男性胸
+		'pec', 'pectoral',
+		// お尻（サイズ指定として）
+		// やはりこれも構図系などが多い...
+		'ass expansion',
+		'bad ass',
+		'flat ass',
+		'huge ass',
+		'spread ass',
+		'butt crack',
+		'wedgie',
+		'wide hips',
+		// 唇色
+		//（唇の動きをキャンセルする必要がある）
+		'lip',
+		// 舌個別
+		'long tongue',
+		// 歯系
+		'teeth', 'fang', 'tusk', 'skin fang',
+		// 鎖骨、見える構図とかっぽいが...
+		'collarbone',
+		// 足
+		'slim legs',
+		// 腰
+		'waist',
+		// 翼
+		'wing',
+		// 肌色
+		'skin',
+		'Tan',
+		'Tanlines',
+		'Sun tattoo',
+	]
+	// body accessories
+	// hand, mouthはポージングが多い
+	// https://danbooru.donmai.us/wiki_pages/tag_group%3Aattire
+	// https://danbooru.donmai.us/wiki_pages/tag_group%3Adress
+	const crothWords = [
+		// その他
+		'ribbon',
+		'lace',
+		// 服
+		'sweat', 'sailor', 'serafuku', 'fuku', 'school uniform', 'skirt', 'sleeve', 'pantyhose', 'pant', 'Aglove', 'frill',
+		'cuffs',
+		'garter', 'strap',
+		'garter straps',
+		// ドレス
+		'dress',
+		'tied dress',
+		'crinoline',
+		'dirndl',
+		'evening gown',
+		'gown',
+		'negligee',
+		'nightgown',
+		'sundress',
+		//
+		'shirt',
+		// 髪飾り
+		// https://danbooru.donmai.us/wiki_pages/tag_group%3Ahair
+		'chopsticks',
+		'comb',
+		'kanzashi',
+		'headband',
+		'scrunchie',
+		'wig',
+		// 頭飾り TODO バニー耳等はデフォルト衣装ではないと分別したいが...
+		'headwear', 'crown', 'fake', 'hat', 'cap',
+		'deerstalker', 'kepi',
+		'bicorne',
+		'ajirogasa',
+		'fedora ',
+		'gat',
+		'tricorne',
+		'deerstalker',
+		'budenovka',
+		'papakha',
+		'ushanka',
+		'bashlik',
+		'toque blanche',
+		'beret',
+		'beanie',
+		'papakha',
+		'ushanka',
+		'kippah',
+		'mian guan',
+		'mitre',
+		'mortarboard',
+		'sajkaca',
+		'songkok',
+		'tam o\' shanter',
+		'tate eboshi',
+		'tsunokakushi',
+		'pelt',
+		// ヘルメット
+		'met',
+		//王冠
+		'circlet',
+		'diadem',
+		'mian guan',
+		'mini crown',
+		'saishi',
+		'tiara',
+		// その他 HeadWare
+		'aviator cap',
+		'balaclava',
+		'bandana',
+		'bonnet',
+		'dalachi',
+		'habit',
+		'head scarf',
+		'hijab',
+		'jester cap',
+		'keffiyeh',
+		'okosozukin',
+		'shower cap',
+		'shufa guan',
+		'visor cap',
+		'veil',
+		// Legware
+		'legwear',
+		// Neck,Neckware
+		'neck',
+		'long neck',
+		'tie',
+		'ascot',
+		'choker',
+		'pet cone',
+		'feather boa',
+		'jabot',
+		'lanyard',
+		'neckerchief',
+		'necklace',
+		'lei',
+		'charm',
+		'pendant',
+		'amulet',
+		'locket',
+		'magatama',
+		'pentacle',
+		'pendant',
+		'amulet',
+		'locket',
+		'magatama',
+		'pentacle',
+		'scarf',
+		'stole',
+		'collared shirt',
+		'detached collar',
+		'fur collar',
+		'high collar',
+		'open collar',
+		'popped collar',
+		'sailor collar',
+		'turtleneck',
+		'sleeveless turtleneck',
+		'v-neck',
+		'wing collar',
+		// sexual
+		'Lingerie',
+		'Babydoll',
+		'Bodystocking',
+		'Bra',
+		'Bustier',
+		'Camisole',
+		'Chemise',
+		'Corset',
+		'Fishnets',
+		'garter straps',
+		'Garter belt',
+		'Lace',
+		'Nightgown',
+		'Panties',
+		'Boyshort panties',
+		'Strapless bottom',
+		'Teddy',
+		'Thighhighs',
+		'Thong',
+		'G-string',
+		'Pearl thong',
+		'Male Underwear',
+		'Boxers',
+		'Briefs',
+		'Boxer briefs',
+		'Bikini briefs',
+		'Jockstrap',
+		'Ball bra',
+		'Penis sheath',
+		'Blindfold',
+		'Bodysuit',
+		'Gimp suit',
+		'bondage outfit',
+		'Latex',
+		'Monoglove',
+		'Crotchless',
+		'crotchless panties',
+		'crotchless pants',
+		'crotchless swimsuit',
+		'crotchless pantyhose',
+		'crotchless leotard',
+		'crotchless bloomers',
+		'crotchless buruma',
+		'Ass Cutout',
+		'assless swimsuit',
+		'backless panties',
+		'backless pants',
+		'Breasts',
+		'breastless clothes',
+		'nippleless clothes',
+		'cupless bikini',
+		'cupless bra',
+		'revealing clothes',
+		'reverse outfit',
+		'reverse bikini armor',
+		'reverse bunnysuit',
+		'anal ball wear',
+		'Maebari',
+		'Pasties',
+		// 袖
+		'sleeve',
+		// メガネ
+		'glasses',
+		'monocle',
+		'sunglasses',
+		'goggles',
+		'scouter',
+		//
+		'glove',
+		//
+		'boots', 'footwear',
+		//
+		'chief',
+		'bare shoulders',
+		'collar',
+	]
+	// あくまで赤髪とかで取りこぼしがあった場合に拾うため程度のもの。
+	//（本当に色指定系を集めだすと、グラデなどの背景指定が入ってきてしまうため）
+	const colorWords = [
+		'aqua', 'black', 'brown', 'blue', 'green', 'gray', 'orange', 'pink', 'purple', 'red', 'white', 'yellow',
+		'brond', 'silver', 'gold'
+	]
+	// 取り除く系のリスト
+	const otherWords = [
+		//
+		'1girl', '2girls', '3girls',
+		'1boy', '2boys', '3boys',
+		'multiple boys', 'multiple girls',
+		// ** Body系
+		// close eyeなどを外見特徴から除外
+		'close', 'open',
+		// 顔、口等は、外見というより表情などが多い
+		// 「うなじ」「肩」が見えるポーズとか
+		'mouse', 'face', 'nape', 'shoulders', 'tongue',
+		'neck', 'collarbone', 'nape',
+		// 唇色以外の唇の動きを取り除く
+		'Cum on lips',
+		'Licking lips',
+		'Lip biting',
+		'Open mouth',
+		'Parted lips',
+		'Puckered lips',
+		'Pursed lips',
+		'Spread lips',
+		// ** 服飾系
+		// ドレス（エフェクト的なタグが多い）
+		'dress flip',
+		'dress grab',
+		'dress lift',
+		'dress pull',
+		'dress tug',
+		'open dress',
+		'skirt basket',
+		'torn dress',
+		'wet dress',
+		// スカートアクション
+		'holding skirt',
+		'skirt around one leg',
+		'skirt basket',
+		'skirt fan',
+		'skirt flip',
+		'skirt hold',
+		'skirt lift',
+		'skirt pull',
+		'skirt removed',
+		'skirt tied over head',
+		'skirt tug',
+		'teppeki skirt',
+		'upskirt',
+		'under skirt',
+		// Neck,Neckware action
+		'arm around neck',
+		'arms around neck',
+		'hand on another\'s neck',
+		'hand on own neck',
+		'neck biting',
+		'necking',
+		'kissing neck',
+		'grab',
+		'tug',
+		'adjust',
+		// Sleeve action
+		'arm out of sleeve',
+		'hands in opposite sleeves',
+		'pinching sleeves',
+		'pinching sleeves',
+		'sleeve grab',
+		// 眼鏡のその他
+		// 普段眼鏡をかけていないキャラクターが眼鏡をかけている場合。
+		'bespectacled',
+		//
+		'adjusting eyewear',
+		'crooked eyewear',
+		'eyewear on head',
+		'eyewear on headwear',
+		'eyewear removed',
+		'eyewear hang',
+		'eyewear in mouth',
+		'holding eyewear',
+		'looking for glasses',
+		'eyewear strap',
+		'eyewear switch',
+		'eyewear view',
+		'hand on eyewear',
+		'looking over eyewear',
+		'no eyewear',
+		'removing eyewear',
+		// 手袋アクション
+		'adjusting gloves',
+		'glove biting',
+		'glove in mouth',
+		'glove pull',
+		'holding gloves',
+		'putting on gloves',
+		'removing glove',
+		//
+		'boots removed',
+		'putting on boots',
+		'single boot',
+		'single thigh boot',
+		'single knee boot',
+		'thighhighs under boots',
+		'torn boots',
+		//
+		'cum', 'facial',
+		'heart',
+		'blush',
+		'alternate costume'
+	]
 
+	const isArrayIncludes = (target, arr) => arr.some(el => target.includes(el));
+	const isArrayEquals = (target, arr) => arr.some(el => target === el);
+
+	let bodys = []
+	let croths = []
+	let colors = []
+	let others = []
+	let tothers = []
+	for( let i = 0; i < tags.length; i++){
+		const tag = tags[i]
+		if(isArrayIncludes(tag, otherWords)){
+			tothers.push(tag)
+		}else if(isArrayIncludes(tag, bodyWords)){
+			bodys.push(tag)
+		}else if(isArrayIncludes(tag, crothWords)){
+			croths.push(tag)
+		}else if(isArrayIncludes(tag, colorWords)){
+			colors.push(tag)
+		}else{
+			tothers.push(tag)
+		}			
+	}
+	// その他に分類されたタグから、完全一致するタグを移動しなおして末尾に付ける
+	for( let i = 0; i < tothers.length; i++){
+		const tag = tothers[i]
+		if(isArrayEquals(tag, otherWords)){
+			others.push(tag)
+		}else if(isArrayEquals(tag, bodyWords)){
+			bodys.push(tag)
+		}else if(isArrayEquals(tag, crothWords)){
+			croths.push(tag)
+		}else if(isArrayEquals(tag, colorWords)){
+			colors.push(tag)
+		}else{
+			others.push(tag)
+		}			
+	}
+
+	let dsttags = []
+	dsttags.push(bodys)
+	dsttags.push(croths)
+	dsttags.push(colors)
+	dsttags.push(others)
+
+	console.log('dsttags')
+	console.log(dsttags)
+	return dsttags
+}
+}
